912388e838b7002362c4b05955c7995bbbe3930f
rogueboi <bondionicbond626@gmail.com>
2021-06-26 21:20:34 +0530

    Changes Made by rogueboi!!

--- app/src/main/java/com/andc/andcsocials/RegisterUserInformation.java
+++ app/src/main/java/com/andc/andcsocials/RegisterUserInformation.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class societyMenu extends AppCompatActivity {
+public class RegisterUserInformation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_society_menu);
+        setContentView(R.layout.activity_register_user_information);
     }
 }
