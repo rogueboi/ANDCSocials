b8eb5e772b4f5e4c46c6ef1c01cdcbfe384fc149
aniketroy420 <aniketroy2002@gmail.com>
2021-06-27 15:54:32 +0530

    Added society_menu first one is made with card view the the other ones are normal made by relative layout

---
+++ app/src/main/java/com/andc/andcsocials/society_menu.java
@@ -0,0 +1,14 @@
+package com.andc.andcsocials;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class society_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_society_menu);
+    }
+}
